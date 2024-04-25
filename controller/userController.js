e981ce75e90cb431c9cc785087e8f67b37c77a6e
8948390097 <singhchauhan863@gmail.com>
2024-04-25 16:57:58 +0530

    add all files

---
+++ controller/userController.js
@@ -0,0 +1,28 @@
+const { request, response } = require('express')
+const db = require('../dataBaseConfig.js')
+
+
+exports.saveData= (req, res) =>{
+    let name = req.body.name
+    let email = req.body.email
+
+    let value = [[name, email]]
+    let sql = 'insert into user(name, email) values ?'
+    db.query(sql, [value], (err, result)=>{
+        if(err) throw err
+        else{
+            res.send("data submitted")
+        }
+    })
+}
+
+exports.getData = (request, response)=>{
+    let sql = "select * from user"
+
+    db.query(sql,(err, result)=>{
+        if(err) throw err
+        else{
+            response.json(result)
+        }
+    })
+}
